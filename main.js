dd4d4a38afdf6cb14e794da26d9197e0a6163446
Brent Nygaard <nygaardmeister@gmail.com>
2016-04-05 17:48:30 +0200

    Sort by file size and display in descending order.

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 var fs = require('fs');
+var dirToRead = 'test/';
 
 function getFileSize(filename) {
   'use strict';
   
-  var stats = fs.statSync('test/' + filename),
-    fileSize = stats.size;
-  return fileSize;
+  var stats = fs.statSync(dirToRead + filename); // create an object storing all of the specified file's stats.
+  return stats.size; // get just the size of the specified file.
   
 }
 
-fs.readdir('test/', function (err, files) {
+fs.readdir(dirToRead, function (err, files) { // read a specified directory and load the file names as an array 'files'.
   'use strict';
   
+  var sorted = []; // empty array to use for sorting later.
+  
   if (err) {
-    console.error(err);
+    console.error(err); // catch and display any errors from the fs module.
   }
   
-  for (var i = 0; i < files.length; i++) {
-    console.log(files[i] + ' is ' + getFileSize(files[i]) + ' bytes.');
+  for (var x = 0; x < files.length; x++) {
+    sorted[x] = {name: files[x], size: getFileSize(files[x])}; // create an object in 'sorted' for each file name in the files array and assign name and size keys and values.
+  }
+  
+  sorted.sort(function (a, b) { // sort the new array by file size in descending order.
+    return b.size - a.size;
+  });
+  
+  for (var i = 0; i < sorted.length; i++) {
+    console.log(sorted[i].name + ' = ' + sorted[i].size + ' bytes' + '\n');
   }
   
 });
